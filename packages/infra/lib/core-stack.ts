a538cbea159dd079019cb0d7cf0067e785393320
Jake Ailor <ailor@amazon.com>
2025-12-10 15:48:56 -0500

    feat(phase-02-ticket-05): add health lambda to core stack

--- packages/infra/lib/core-stack.ts
+++ packages/infra/lib/core-stack.ts
@@ -1,5 +1,8 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class CoreStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -13,6 +16,16 @@ export class CoreStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'AssetsBucketName', {
       value: assetsBucket.bucketName,
+    });
+
+    const healthFunction = new lambdaNodejs.NodejsFunction(this, 'HealthLambda', {
+      entry: path.join(__dirname, '..', '..', 'backend', 'src', 'lambda', 'healthHandler.ts'),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'handler',
+    });
+
+    new cdk.CfnOutput(this, 'HealthLambdaName', {
+      value: healthFunction.functionName,
     });
   }
 }

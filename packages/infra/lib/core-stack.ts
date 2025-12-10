8582ecb2a198a78f18fa6fa73ddd72fe65bc641b
Jake Ailor <ailor@amazon.com>
2025-12-10 14:48:06 -0500

    chore(phase-02-ticket-03): add core assets S3 bucket with output

--- packages/infra/lib/core-stack.ts
+++ packages/infra/lib/core-stack.ts
@@ -1,8 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class CoreStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // Resources will be defined in later phases.
+
+    const assetsBucket = new s3.Bucket(this, 'AssetsBucket', {
+      versioned: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    new cdk.CfnOutput(this, 'AssetsBucketName', {
+      value: assetsBucket.bucketName,
+    });
   }
 }
